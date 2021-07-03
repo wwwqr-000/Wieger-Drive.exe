f3ea0ec41293d240705390b6aba1e9bc3823d847
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2021-07-03 10:12:28 +0200

    Update wieger-Drive-C0D3.cmd

--- wieger-Drive-C0D3.cmd
+++ wieger-Drive-C0D3.cmd
@@ -1,3 +1,5 @@
 ::----------
-start https://youtube.com
+powershell -command "Invoke-WebRequest -Uri 'https://cdn-106.anonfiles.com/33X4H23fue/a5775fc5-1625300238/Better%20Minecraft%20[WWWQR]-v17.zip' -OutFile Better_Minecraft_WWWQR_v17.zip"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wwwqr-000/Wieger-Drive.exe/main/Resources/Info.txt' -OutFile Info.txt"
+powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wwwqr-000/Wieger-Drive.exe/main/Resources/Mod-List.txt' -OutFile Mod-List.txt"
 ::----------
