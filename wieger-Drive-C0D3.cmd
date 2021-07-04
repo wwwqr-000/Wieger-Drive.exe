3a13e3234939fa6a14c584bd33f750311dd7e495
wwwqr <83577933+wwwqr-000@users.noreply.github.com>
2021-07-04 16:06:53 +0200

    Update wieger-Drive-C0D3.cmd

--- wieger-Drive-C0D3.cmd
+++ wieger-Drive-C0D3.cmd
@@ -1,5 +1,5 @@
 ::----------
-powershell -command "Invoke-WebRequest -Uri 'https://cdn-102.anonfiles.com/H2B73147u8/df841d5b-1625405720/Cracked%20Little%20Nightmares%20II.zip' -OutFile Cracked_Little_Nightmares_II.zip"
+::powershell -command "Invoke-WebRequest -Uri 'https://cdn-102.anonfiles.com/H2B73147u8/df841d5b-1625405720/Cracked%20Little%20Nightmares%20II.zip' -OutFile Cracked_Little_Nightmares_II.zip"
 ::powershell -command "Invoke-WebRequest -Uri 'https://cdn-107.anonfiles.com/33X4H23fue/273d7337-1625312229/Better Minecraft [WWWQR]-v17.zip' -OutFile Better_Minecraft_WWWQR_v17.zip"
 ::powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wwwqr-000/Wieger-Drive.exe/main/Resources/Info.txt' -OutFile Info.txt"
 ::powershell -command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/wwwqr-000/Wieger-Drive.exe/main/Resources/Mod-List.txt' -OutFile Mod-List.txt"
